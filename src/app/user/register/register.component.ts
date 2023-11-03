1a9a5d56da220803c59207e76e5d06601c2d25e5
cheduc1 <chevanminhduc@gmail.com>
2023-11-03 10:59:34 +0700

    done

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -7,32 +7,35 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
+  showAlert = false;
+  alertMsg = 'Your account is creating!';
+  alertColor = '';
+
   registerForm = new FormGroup({
-    name: new FormControl('',[
-      Validators.required,
-      Validators.minLength(3)
-    ]),
-    email: new FormControl('',[
-      Validators.required,
-      Validators.email
-    ]),
-    age: new FormControl('',[
-      Validators.required,
-      Validators.min(18),
-      Validators.max(100)
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    age: new FormControl('', [
+      // Validators.required,
+      // Validators.min(18),
+      // Validators.max(100)
     ]),
     password: new FormControl('', [
       Validators.required,
-      Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm
+      ),
     ]),
-    confirm_password: new FormControl('', [
-      Validators.required,
-      
-    ]),
-    phoneNumber: new FormControl('',[
+    confirm_password: new FormControl('', [Validators.required]),
+    phoneNumber: new FormControl('', [
       Validators.required,
       Validators.minLength(13),
-      Validators.maxLength(13)
+      Validators.maxLength(13),
     ]),
   });
+
+  register() {
+    this.showAlert = true;
+    this.alertColor = 'blue';
+    this.alertMsg = 'Your account is creating!';
+  }
 }
